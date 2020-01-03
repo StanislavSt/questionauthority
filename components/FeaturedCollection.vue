376a84d8115c1469df25f6a09dadce45b7124538
StanislavSt <s.staev1@gmail.com>
2020-01-03 18:04:40 +0100

    Prototyping new layout

--- components/FeaturedCollection.vue
+++ components/FeaturedCollection.vue
@@ -1,8 +1,14 @@
 <template>
 	<div class="section-featured-collection">
 		<div class="featured-collection">
-			<a class="section-right"></a>
-			<a class="section-left"></a>
+			<nuxt-link class="section-right" to="/products">
+				<span class="span-shop">Kiril Kiryakov x Question Authority</span>
+				<!--<button class="btn-shop">Shop</button>-->
+			</nuxt-link>
+			<nuxt-link class="section-left" to="/products">
+				<span class="span-shop">Kiril Kiryakov x Question Authority</span>
+				<!--<button class="btn-shop">Shop</button>-->
+			</nuxt-link>
 		</div>
 	</div>
 </template>
@@ -12,35 +18,54 @@ export default {};
 </script>
 
 <style scoped>
+.btn-shop {
+	background: transparent;
+	color: white;
+	height: 40px;
+	width: 120px;
+	border: 2px solid black;
+	cursor: pointer;
+}
+.span-shop {
+	position: absolute;
+	bottom: 0;
+	left: 0;
+	color: white;
+	background-color: black;
+	margin-left: 5px;
+}
 .section-featured-collection {
-	height: 900px;
+	margin-top: 2.3rem;
+	height: 40vw;
 	width: 100%;
 }
 .featured-collection {
 	height: 100%;
 	width: 100%;
 	display: flex;
+	border: 2px solid black;
+}
+.section-right,
+.section-left {
+	position: relative;
+	background-repeat: no-repeat;
+	background-size: cover;
+	background-position: center top;
+	transition: all 0.4s ease;
+	cursor: pointer;
 }
 .section-right {
 	flex: 1;
 	order: 2;
-	background-repeat: no-repeat;
-	background-size: cover;
-	background-position: center top;
 	background-image: url("../assets/Kiril/Urb/Negative0-12-11.jpg");
-	transition: all 0.4s;
+	border-left: 2px solid black;
 }
 .section-left {
 	flex: 2;
 	order: 1;
-	background-repeat: no-repeat;
-	background-size: cover;
-	background-position: center top;
 	background-image: url("../assets/Kiril/Urb/Negative0-21-20.jpg");
-	transition: all 0.4s;
 }
 .section-right:hover ~ .section-left {
 	flex: 1;
-	flex-shrink: 1;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
 	<div>
-		<News :blok="collections" />
+		<FeaturedCollection />
+		<div class="text">Latest Collections</div>
+		<CollectionPreview :blok="stories" />
+		<CollectionPreview2 :blok="stories" />
 		<InstagramGallery />
 		<InformationLogo />
 	</div>
@@ -11,20 +14,72 @@ import InstagramGallery from "@/components/InstagramGallery.vue";
 import News from "@/components/News.vue";
 import InformationLogo from "@/components/InformationLogo.vue";
 import ProductCarousel from "@/components/ProductCarousel.vue";
+import FeaturedCollection from "@/components/FeaturedCollection.vue";
+import CollectionPreview from "@/components/CollectionPreview.vue";
+import CollectionPreview2 from "@/components/CollectionPreview2.vue";
 import { getPosts } from "@/api";
 export default {
 	components: {
 		InstagramGallery,
 		News,
 		InformationLogo,
-		ProductCarousel
+		ProductCarousel,
+		FeaturedCollection,
+		CollectionPreview,
+		CollectionPreview2
 	},
-	async asyncData(context) {
-		const posts = await getPosts(context);
-		return { collections: posts };
+	asyncData(context) {
+		// Check if we are in the editing mode
+		let editMode = false;
+		if (
+			context.query._storyblok ||
+			context.isDev ||
+			(typeof window !== "undefined" &&
+				window.localStorage.getItem("_storyblok_draft_mode"))
+		) {
+			if (typeof window !== "undefined") {
+				window.localStorage.setItem("_storyblok_draft_mode", "1");
+				if (window.location == window.parent.location) {
+					window.localStorage.removeItem("_storyblok_draft_mode");
+				}
+			}
+			editMode = true;
+		}
+		return context.app.$storyapi
+			.get(`cdn/stories/`, {
+				starts_with: "products/",
+				version: editMode ? "draft" : "published",
+				cv: context.store.state.cacheVersio
+			})
+			.then(res => {
+				return res.data;
+			})
+			.catch(res => {
+				if (!res.response) {
+					console.error(res);
+					errorCallback({
+						statusCode: 404,
+						message: "Failed to receive content from the api."
+					});
+				} else {
+					console.error(res.response.data);
+					errorCallback({
+						statusCode: res.response.status,
+						message: res.response.data
+					});
+				}
+			});
 	}
 };
 </script>
 
 <style scoped>
+@import url("https://fonts.googleapis.com/css?family=Raleway:600,900&display=swap");
+.text {
+	font-family: "Raleway", sans-serif;
+	margin-top: 5rem;
+	text-align: center;
+	font-size: 32px;
+	text-transform: uppercase;
+}
 </style>
